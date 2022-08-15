5ef1d6e195482becffc6d30263d089233a483ef8
FinnHorner <finhor35@kingshigh.school.nz>
2022-08-15 13:24:58 +1200

    Monday

---
+++ SpinningBlade.gd
@@ -0,0 +1,14 @@
+extends Path2D
+export (int) var follow_speed = 50
+
+func _ready():
+	$AnimationPlayer.play ("spin")
+	
+func _process(delta):
+	$Path2D/PathFollow2D.offset += follow_speed * delta
+
+
+
+
+
+
