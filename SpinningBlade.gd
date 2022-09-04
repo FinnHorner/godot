7d4a6b94d5e0b6f98286efc69936ff8371a39ab0
FinnHorner <finhor35@kingshigh.school.nz>
2022-09-05 09:43:50 +1200

    Monday

--- SpinningBlade.gd
+++ SpinningBlade.gd
@@ -1,19 +1,10 @@
 extends Node2D
 export (int) var follow_speed = 50
 
-func _ready():
-	$AnimationPlayer.play ("spin")
+
 	
 func _process(delta):
 	$Path2D/PathFollow2D.offset += follow_speed * delta
-
-
-func _on_Area2D_body_entered(body):
-	print(body)
-	if body.is_in_group("Player"):
-		print("Player Dead")
-		if GameStats.check_reset() == false:
-			body.global_position = GameStats.get_spawn().global_position
 
 
 
